c2cc9212d99840bfba165ee6a2513e07893771e3
David <digital0david@gmail.com>
2019-05-30 21:25:50 -0400

    routers

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const server = express();
 
-// const CohortRouter = require('./users/userRouter.js');
-// const StudentRouter = require('./posts/postRouter.js');
+const CohortRouter = require('./data/cohort/cohortRouter.js');
+const StudentRouter = require('./data/student/studentRouter.js');
 
 server.use(express.json());
+server.use('/api/cohorts', CohortRouter);
+server.use('/api/students', StudentRouter);
 
 module.exports = server;
