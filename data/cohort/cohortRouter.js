caaac354d7cf3e7394386bd3d9cd16a9ae7e031d
David <digital0david@gmail.com>
2019-05-30 23:17:58 -0400

    post and get

--- data/cohort/cohortRouter.js
+++ data/cohort/cohortRouter.js
@@ -5,4 +5,35 @@ const knex = require('knex');
 const knexConfig = require('../../knexfile.js');
 const db = knex(knexConfig.development);
 
+router.post('/', (req, res) => {
+    const { name } = req.body;
+
+    if(!name) {
+        res.status(400).json({
+            message: "Please provide a name for the cohort."
+        })
+        return;
+    }
+
+    db.insert(req.body, ['id'])
+        .into('cohorts')
+        .then(id => {
+            res.status(201).json(id)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+});
+
+router.get('/', (req, res) => {
+    db('cohorts')
+        .then(cohorts => {
+            res.status(200).json(cohorts)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+})
+
 module.exports = router;
