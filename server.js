fe4b2490273a0011bbefd15f32099ae9ce5a5db0
David <digital0david@gmail.com>
2019-05-30 21:44:29 -0400

    db/migration for cohorts

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const server = express();
+const helmet = require('helmet');
 
 const CohortRouter = require('./data/cohort/cohortRouter.js');
 const StudentRouter = require('./data/student/studentRouter.js');
 
 server.use(express.json());
+server.use(helmet());
 server.use('/api/cohorts', CohortRouter);
 server.use('/api/students', StudentRouter);
 
